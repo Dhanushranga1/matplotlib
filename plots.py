32d60f518193b4ae779cf1262c3bfa59f08fc595
Dhanush Ranga <gdr20704@gmail.com>
2024-05-04 18:28:00 +0530

    jhuu
    hhhihuiu

---
+++ plots.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+
+# fig, ax = plt.subplots()
+
+# ax.plot([1, 2, 3, 4], [1, 4, 2, 3]) 
+# ax.set_title('basic plot')
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.grid(True)
+# ax.scatter([4,3,2,1,],[3,2,4,5], color='red')
+
+
+# plt.show()
+fig = plt.figure()
+fig, axs = plt.subplots(2, 2)
+plt.show()
+hbhbh
